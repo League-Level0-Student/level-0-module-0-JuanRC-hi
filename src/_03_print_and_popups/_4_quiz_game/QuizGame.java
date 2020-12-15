51de8a8bd96553084770726573be4ff1754a9352
root <root@110f67da4b35>
2020-12-15 23:35:55 +0000

    awesome

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,17 +1,40 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+	int score = 0;	
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+	
+	String A = JOptionPane.showInputDialog("Whats 5x2?");
 				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
+if  (A.equals("10")){ 
+	score++;
+	JOptionPane.showMessageDialog(null, "correct");
+	
+}
+else {
+	score--;
+}
+JOptionPane.showMessageDialog(null, score);	
+
+String B = JOptionPane.showInputDialog(" Whats 10x10");
+if (B.equals("100")){
+	score++;
+	JOptionPane.showMessageDialog(null, "correct");
+}
+
+else {
+	score--;
+}
+JOptionPane.showMessageDialog(null, score);				
+
+// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,25 +2,30 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
+public static void main(String[] args) {
+	
 
 	
 		Random ran = new Random();    //This will be used below to make a random number. 
 		
 		// 2. Make a variable that will hold a random whole number
-	
+int random = 10;	
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
-	
+random = ran.nextInt(4);
 		// 3. Print your variable to the console
-	
+System.out.print(random);
 		// 4. Get the user to enter something that they think is awesome
-	
+String A = JOptionPane.showInputDialog("what's something awesome?");
 		// 5. If your variable is  0
-	
+	if (random== 0){
 			// -- tell the user whatever they entered is awesome!
-	
+	JOptionPane.showMessageDialog(null, "It's awesome");
+	}
 		// 6. If your variable is  1
 	
 			// -- tell the user whatever they entered is ok.
@@ -33,4 +38,5 @@ public class AwesomeOrNot {
 	
 			// -- invent your own message to give to the user (be nice).
 
+}
 }
